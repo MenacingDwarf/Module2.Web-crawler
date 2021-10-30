3a84616d37deb4ff1ff7d8b7a494c0673ff0d51b
MenacingDwarf <MenacingDwarf@yandex.ru>
2021-10-30 13:21:43 +0300

    Add another statistics for crawling; Speed up crawling;

--- WebCrawler.py
+++ WebCrawler.py
@@ -3,6 +3,7 @@ from urllib.parse import urljoin
 import requests
 from bs4 import BeautifulSoup
 import sys
+import time
 
 class UrlInfo:
     def __init__(self, url, is_internal, is_valid):
@@ -17,6 +18,7 @@ class WebCrawler:
         self.urls_to_visit = [url]
         self.full_urls = []
         self.url_index = 0
+        self.total_externals = 0
 
     def download_url(self, url):
         r = requests.get(url)
@@ -30,78 +32,102 @@ class WebCrawler:
                 path = urljoin(url, path)
             yield path
 
-    def add_url_to_visit(self, url):
-        if not url in self.visited_urls and not url in self.urls_to_visit:
-            self.urls_to_visit.append(url)
-    
-    def crawl(self, url):
+    def crawl(self, url, log=True):
         html, success = self.download_url(url)
+        if log:
+            logging.info(f'---- URL status: {"True" if success else "False"}')
+
         if not success:
             self.full_urls.append(UrlInfo(url, True, False))
             return
-        
+
         self.visited_urls.append(url)
         if url != self.main_url:
             self.full_urls.append(UrlInfo(url, True, True))
-        
+
         for new_url in self.get_linked_urls(url, html):
-            if not (new_url.startswith("https://") or new_url.startswith("http://")) or new_url == url:
+            if new_url is None or not (new_url.startswith("https://") or new_url.startswith("http://")) or new_url == url:
                 continue
-                
+
             if self.is_internal(new_url):
-                self.add_url_to_visit(new_url)
+                if not new_url in self.visited_urls and not new_url in self.urls_to_visit:
+                    self.urls_to_visit.append(new_url)
+                    if log:
+                        logging.info(f'---- New internal URL: {new_url}')
             else:
-                try:
-                    _, valid = self.download_url(new_url)
-                    if len(list(filter(lambda item : item.url == new_url, self.full_urls))) == 0:
+                self.total_externals = self.total_externals + 1
+                if len(list(filter(lambda item : item.url == new_url, self.full_urls))) == 0:
+                    try:
+                        _, valid = self.download_url(new_url)
                         self.full_urls.append(UrlInfo(new_url, False, valid))
-                except Exception:
-                    if len(list(filter(lambda item : item.url == new_url, self.full_urls))) == 0:
+                        if log:
+                            logging.info(f'---- New external URL: {new_url}, valid: {"True" if valid else "False"}')
+                    except Exception:
                         self.full_urls.append(UrlInfo(new_url, False, False))
-                    pass
-            
+                        if log:
+                            logging.info(f'---- New external URL: {new_url}, valid: False')
+
     def is_internal(self, url):
         return url is not None and url.startswith(self.main_url)
 
-    def run(self, debug=True):
+    def run(self, debug=True, log=True):
+        start_time = time.time()
+        if log:
+            logging.basicConfig(filename='crawler.log', encoding='utf-8', level=logging.INFO)
+            logging.info(f'Start crawling {self.main_url}...')
+            logging.info(f'-----------------------------------')
         while self.urls_to_visit:
             self.url_index = self.url_index + 1
-            if debug:
-                print(f'Crawling: {self.url_index} of {len(self.urls_to_visit) + len(self.visited_urls)}')
-            
             url = self.urls_to_visit.pop(0)
-            logging.info(f'Crawling {url}: {self.url_index} of {len(self.urls_to_visit) + len(self.visited_urls) + 1}')
+
+            if debug:
+                print(f'Crawling: {self.url_index} of {len(self.urls_to_visit) + len(self.visited_urls) + 1}')
+            if log:
+                logging.info(f'Crawling {url}: {self.url_index} of {len(self.urls_to_visit) + len(self.visited_urls) + 1}')
+
             try:
-                self.crawl(url)
+                self.crawl(url, log)
             except Exception:
-                logging.exception(f'Failed to crawl: {url}')
-        
+                if log:
+                    logging.exception(f'Failed to crawl: {url}')
+
+        if log:
+            logging.info(f'-----------------------------------')
+            logging.info(f'Crawling end with {time.time() - start_time} seconds!\n')
+
     def get_internal_urls(self):
         return list(filter(lambda item : item.is_internal, self.full_urls))
-        
+
     def get_external_urls(self):
         return list(filter(lambda item : not item.is_internal, self.full_urls))
-        
+
+    def get_documents_urls(self):
+        return list(filter(lambda item : item.url.endswith('.doc') or item.url.endswith('.docx') or item.url.endswith('.pdf'), self.full_urls))
+
     def report(self, short = False):
         print('\n--------- CRAWLER REPORT --------\n')
         print(f'Total visited urls: {len(self.full_urls)}')
-        print(f'Total internal urls: {len(self.get_internal_urls())}')
-        print(f'Total external urls: {len(self.get_external_urls())}')
-        
+        print(f'Total unique internal urls: {len(self.get_internal_urls())}')
+        print(f'Total unique external urls: {len(self.get_external_urls())}')
+        print(f'Total external urls: {self.total_externals}')
+        print(f'Total unique document urls: {len(self.get_documents_urls())}')
+
         if short:
             return
-        
+
         print('\n--------- INTERNAL URLS ---------\n')
         for link in self.get_internal_urls():
             print("    OK   " if link.is_valid else "NOT OK   ", link.url)
-            
+
         print('\n--------- EXTERNAL URLS ---------\n')
         for link in self.get_external_urls():
             print("    OK   " if link.is_valid else "NOT OK   ", link.url)
-        
+
+        print('\n--------- DOCUMENT URLS ---------\n')
+        for link in self.get_documents_urls():
+            print("    OK   " if link.is_valid else "NOT OK   ", link.url)
+
 if __name__ == '__main__':
     crawler = WebCrawler(str(sys.argv[1]))
     crawler.run()
     crawler.report()
-    
-    
--- tests.py
+++ tests.py
@@ -6,19 +6,19 @@ class BaseCrawlerTestCase(unittest.TestCase):
     total_num = 0
     internal_num = 0
     external_num = 0
-        
+
     @classmethod
     def setUpClass(cls):
         print(f'Running Test Case: {cls.__name__}...')
         cls.crawler = WebCrawler(cls.url)
-        cls.crawler.run(debug=False)
-        
+        cls.crawler.run(debug=False, log=False)
+
     def testValidTotalNum(self):
         self.assertEqual(len(self.crawler.full_urls), self.total_num)
-        
+
     def testValidInternalsNum(self):
         self.assertEqual(len(self.crawler.get_internal_urls()), self.internal_num)
-        
+
     def testValidExternalsNum(self):
         self.assertEqual(len(self.crawler.get_external_urls()), self.external_num)
 
@@ -27,17 +27,17 @@ class TestPortfolioWebsite(BaseCrawlerTestCase):
     total_num = 22
     internal_num = 9
     external_num = 13
-        
-        
+
+
 class TestSlurpWebsite(BaseCrawlerTestCase):
     url = "https://ramenslurp.ru/"
     total_num = 9
     internal_num = 5
-    external_num = 4        
-        
+    external_num = 4
+
 def get_list_of_tests():
     return list(BaseCrawlerTestCase.__subclasses__())
-    
+
 def run_all_tests():
     loader = unittest.TestLoader()
     cases = []
@@ -46,15 +46,13 @@ def run_all_tests():
     suite = unittest.TestSuite(cases)
     runner = unittest.TextTestRunner()
     runner.run(suite)
-    
-    
+
+
 def run_single_test(test_case):
     loader = unittest.TestLoader()
     suite = unittest.TestSuite([loader.loadTestsFromTestCase(test_case)])
     runner = unittest.TextTestRunner()
     runner.run(suite)
-    
+
 if __name__ == '__main__':
     unittest.main()
-    
-    
